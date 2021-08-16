82dc256d5876d477b5d2d0bca0bd6ea04aa510d4
flaviocubon <flavio_1998@outlook.com>
2021-08-16 11:57:27 -0300

    Implementación ingreso de usuario

--- js/login.js
+++ js/login.js
@@ -1,6 +1,24 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+const buttonLogin = () => {
+    const txtuserEmail = document.getElementById("email");
+    const txtuserPassword= document.getElementById("password");
+    const userEmail = txtuserEmail.value;
+    const userPassword = txtuserPassword.value;
+    localStorage.setItem("email",userEmail);
+    localStorage.setItem("password",userPassword);
+    if (userEmail && userPassword) {
+        window.location = "index.html"
+        localStorage.setItem("logged","true")
+    }
+    else
+    {
+        alert("Debe ingresar datos validos.")
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("login").addEventListener("click", buttonLogin);
 });
